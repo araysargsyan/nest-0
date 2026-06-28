9ee4dbd26d22e8d209c1f73f9401e4520293a535
araysargsyan <araysargsyan96@gmail.com>
2026-06-28 22:26:15 +0400

    test: migrate UserService unit test to user/tests/ subdirectory

--- src/modules/user/tests/user.service.spec.ts
+++ src/modules/user/tests/user.service.spec.ts
@@ -1,5 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { UserService } from './user.service';
+import { UserService } from '../user.service';
 import { PrismaService } from '@modules/shared/prisma/prisma.service';
 
 describe('UserService (Unit)', () => {
